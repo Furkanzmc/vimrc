c12ebc77257ea234ee46758a07bb0b4541fe21fe
Furkan Uzumcu <furkanuzumcu@gmail.com>
2018-11-06 17:54:55 -0500

    backup commit

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -3,6 +3,7 @@ Provides additional functionality for the custom ycm_extra_conf.py files.
 """
 
 import os
+import sys
 
 DIR_OF_THIS_SCRIPT = os.path.abspath(os.path.dirname(__file__))
 SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
@@ -40,15 +41,29 @@ def get_python_conf(**kwargs):
 
     return conf
 
+def contains_headers(file_list):
+    contains = False
+    for file_name in file_list:
+        if file_name.endswith('.h') or file_name.endswith('.hpp'):
+            contains = True
+            break
+
+    return contains
 
 def get_cpp_conf(**kwargs):
 
-    fh = open('/Users/Furkanzmc/Desktop/output.txt', 'w')
-    current_folder_includes = [DIR_OF_THIS_SCRIPT, ]
-    for subdir, dirs, files in os.walk(DIR_OF_THIS_SCRIPT):
-        if dirs:
-            current_folder_includes.append(os.path.join(subdir, dirs[0]))
-        else:
+    fh = open('/Users/uzumcuf/Desktop/output.json', 'w')
+    open_file_dir = None
+    if 'filename' in kwargs:
+        open_file_dir = os.path.dirname(kwargs['filename'])
+
+    dir_to_walk = DIR_OF_THIS_SCRIPT # open_file_dir if open_file_dir is not None else DIR_OF_THIS_SCRIPT
+    current_folder_includes = [dir_to_walk, ]
+    for subdir, dirs, files in os.walk(dir_to_walk):
+        # if dirs:
+        #     current_folder_includes.append(os.path.join(subdir, dirs[0]))
+        # else:
+        if contains_headers(files):
             current_folder_includes.append(subdir)
 
     conf = {
@@ -58,35 +73,53 @@ def get_cpp_conf(**kwargs):
             '-Wall',
             '-Wextra',
             '-Werror',
-            '-v',
+            '-pipe',
+            '-stdlib=libc++',
+            '-g',
+            '-std=gnu++11',
+            '-arch',
+            'x86_64',
+            '-mmacosx-version-min=10.14',
+            '-Wall',
+            '-W',
+            '-fPIC',
+            '-I .',
+            '-I/Users/uzumcuf/Qt/5.10.0/clang_64/lib/QtGui.framework/Headers',
+            '-I/Users/uzumcuf/Qt/5.10.0/clang_64/lib/QtCore.framework/Headers',
+            '-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/OpenGL.framework/Headers',
+            '-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/AGL.framework/Headers',
+            '-I/Users/uzumcuf/Qt/5.10.0/clang_64/mkspecs/macx-clang',
+            '-F/Users/uzumcuf/Qt/5.10.0/clang_64/lib',
+            '-headerpad_max_install_names',
+            '-Wl,-syslibroot,/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk',
+            '-Wl,-rpath,@executable_path/Frameworks',
+            '-Wl,-rpath,/Users/uzumcuf/Qt/5.10.0/clang_64/lib',
+            '-F/Users/uzumcuf/Qt/5.10.0/clang_64/lib',
+            '-framework',
+            'QtGui',
+            '-framework',
+            'QtCore',
+            '-framework',
+            'DiskArbitration',
+            '-framework',
+            'IOKit',
+            '-framework',
+            'OpenGL',
+            '-framework',
+            'AGL',
             '-isystem',
             '/usr/include',
             '-isystem',
             '/usr/local/include',
-            '-isystem',
-            DIR_OF_THIS_SCRIPT,
-            '-I' + DIR_OF_THIS_SCRIPT,
-            '-I.'
-            '-c',
-            '-pipe',
-            '-stdlib=libc++',
-            '-g',
-            '-std=gnu++1y',
-            '-arch',
-            'x86_64',
-            '-isysroot',
-            '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk',
-            '-mmacosx-version-min=10.11',
-            '-Wall',
-            '-W',
-            '-fPIC'
         ],
-        'include_paths_relative_to_dir': current_folder_includes,
-        # 'override_filename': FindCorrespondingSourceFile(kwargs['filename'])
     }
+
+    if 'filename' in kwargs:
+        conf['override_filename'] = FindCorrespondingSourceFile(kwargs['filename'])
 
     for inc in current_folder_includes:
         conf['flags'].append('-I' + inc)
+        conf['flags'].extend(['-isystem', inc])
 
     fh.write(str(conf))
     fh.close()
